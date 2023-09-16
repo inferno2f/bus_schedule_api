c94473998c24d6718bf3fe13a1ed7048e1d4289c
Vlad Nikitin <nikitinv91@gmail.com>
2023-09-16 16:55:58 -0400

    added stop_parser

--- bus_api/api/management/commands/gtfs_parser.py
+++ bus_api/api/management/commands/gtfs_parser.py
@@ -1,25 +1,49 @@
-from django.core.management.base import BaseCommand
-from api.models import Route
-from django.conf import settings
+import csv
 
+from api.models import Route, Stop
+from django.conf import settings
+from django.core.management.base import BaseCommand
 
 STATIC_FOLDER = settings.STATICFILES_DIRS[0]
 
 
 def route_parser():
     with open(f"{STATIC_FOLDER}/routes.txt", "r") as f:
-        next(f)
-        for line in f:
-            columns = line.strip().split(",")
+        csv_reader = csv.reader(f)
+        next(csv_reader)
+        for columns in csv_reader:
             route_id = int(columns[0])
             short_name = columns[2].strip('"')
-            route, created = Route.objects.get_or_create(route_id=route_id)
-            if not created:
-                route.short_name = short_name
-                route.save()
-            route.save()
+            Route.objects.update_or_create(
+                route_id=route_id,
+                defaults={"route_short_name": short_name}
+            )
+
+
+def stop_parser():
+    with open(f"{STATIC_FOLDER}/stops.txt", "r") as f:
+        csv_reader = csv.reader(f)
+        next(csv_reader)
+        for columns in csv_reader:
+            stop_id = int(columns[0])
+            stop_code = int(columns[1])
+            stop_name = columns[2].strip('"')
+            stop_latitude = columns[4].strip('"')
+            stop_longitude = columns[5].strip('"')
+            zone_id = int(columns[6])
+            Stop.objects.update_or_create(
+                stop_id=stop_id,
+                defaults={
+                    "stop_code": stop_code,
+                    "stop_name": stop_name,
+                    "stop_latitude": stop_latitude,
+                    "stop_longitude": stop_longitude,
+                    "zone_id": zone_id,
+                }
+            )
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         route_parser()
+        stop_parser()
