d3d46a9383a010a3a10ecea3234581ef4d4ddcbd
Vlad Nikitin <nikitinv91@gmail.com>
2023-09-17 11:09:44 -0400

    list and retrieve for routes

--- bus_api/api/views.py
+++ bus_api/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, response
 from api.serializers import RouteSerializer
 from api.models import Route
 
@@ -6,3 +7,14 @@ from api.models import Route
 class RouteViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
+
+    def list(self, request):
+        queryset = Route.objects.all()
+        serializer = RouteSerializer()(queryset, many=True)
+        return response.Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Route.objects.all()
+        route = get_object_or_404(queryset, pk=pk)
+        serializer = RouteSerializer(route)
+        return response.Response(serializer.data)
