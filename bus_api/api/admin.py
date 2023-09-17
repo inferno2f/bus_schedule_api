3bd2c1900c9e29a4ceb68d8976d22db46473be35
Vlad Nikitin <nikitinv91@gmail.com>
2023-09-17 11:09:15 -0400

    added search fields for routes

--- bus_api/api/admin.py
+++ bus_api/api/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from api.models import Route, Stop, StopTimes, CalendarDates, Trip
 
-admin.site.register(Route)
+
+class RouteAdmin(admin.ModelAdmin):
+    search_fields = ("route_short_name",)
+
+
+admin.site.register(Route, RouteAdmin)
 admin.site.register(Stop)
 admin.site.register(CalendarDates)
 admin.site.register(Trip)
