483e4429b6734de315f6a7fed795e2465e34c941
Vlad Nikitin <nikitinv91@gmail.com>
2023-09-20 13:48:19 -0400

    performance improvements

--- bus_api/api/admin.py
+++ bus_api/api/admin.py
@@ -6,8 +6,13 @@ class RouteAdmin(admin.ModelAdmin):
     search_fields = ("route_short_name",)
 
 
+class StopTimesAdmin(admin.ModelAdmin):
+    """Significantly improves performance of the admin panel"""
+    raw_id_fields = ("trip", "stop",)
+
+
 admin.site.register(Route, RouteAdmin)
 admin.site.register(Stop)
 admin.site.register(CalendarDates)
 admin.site.register(Trip)
-admin.site.register(StopTimes)
+admin.site.register(StopTimes, StopTimesAdmin)
