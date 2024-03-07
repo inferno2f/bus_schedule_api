68d6db25e7086bacb589b85a67659125897d3553
Vlad Nikitin <nikitinv91@gmail.com>
2024-03-07 18:29:56 -0500

    filter fields added

--- bus_api/api/admin.py
+++ bus_api/api/admin.py
@@ -8,16 +8,20 @@ class RouteAdmin(admin.ModelAdmin):
 
 class StopTimesAdmin(admin.ModelAdmin):
     """Significantly improves performance of the admin panel"""
-    raw_id_fields = ("trip", "stop",)
-    search_fields = ("trip__trip_id", "stop__stop_id")
+    raw_id_fields = ("trip",)
+    search_fields = ("=trip__trip_id",)
 
 
 class StopAdmin(admin.ModelAdmin):
     search_fields = ("stop_name", "stop_id")
 
 
+class TripAdmin(admin.ModelAdmin):
+    search_fields = ("=route__route_id", "=trip_id",)
+
+
 admin.site.register(Route, RouteAdmin)
 admin.site.register(Stop, StopAdmin)
 admin.site.register(CalendarDates)
-admin.site.register(Trip)
+admin.site.register(Trip, TripAdmin)
 admin.site.register(StopTimes, StopTimesAdmin)
